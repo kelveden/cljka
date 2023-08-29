777cb791329f1369dc7c70d9dce3588ef7a76fef
Alistair Dutton <kelveden@gmail.com>
2023-08-29 18:56:19 +0100

    Started channel work.

--- src/cljka/channel.clj
+++ src/cljka/channel.clj
@@ -1,8 +1,70 @@
 (ns cljka.channel
   (:require [clojure.core.async :as async]
-            [clojure.core.async.impl.protocols :as async-protocols]))
+            [clojure.core.async.impl.protocols :as async-protocols]
+            [clojure.spec.alpha :as s]
+            [clojure.tools.logging :as log])
+  (:import (clojure.lang Atom)))
+
+(s/def ::channel #(satisfies? async-protocols/Channel %))
 
 (defn closed?
   "Returns a boolean indicating whether the specified channel is closed."
   [ch]
   (async-protocols/closed? ch))
+
+(defn- loop-channel-to-sink
+  [channel sink-fn {:keys [pred n] :or {pred any?
+                                        n    nil}}]
+  (try
+    (loop [next            (async/<!! channel)
+           processed-count 0]
+      (when next
+        (let [process?  (pred next)
+              new-count (cond-> processed-count process? inc)]
+          (when process? (sink-fn next))
+
+          ; Limit looping to n invocations of f
+          (when (or (nil? n) (< new-count n))
+            (recur (async/<!! channel) new-count)))))
+
+    (catch Exception e
+      (log/error e))))
+
+(defmulti sink
+          "Creates a new sink channel to the specified target.
+
+          | key                  | default | description |
+          |:---------------------|:--------|:------------|
+          | `:n`                 | `nil`   | A limit to the number of messages that will be written to the sink before it closes. |"
+          type)
+
+(defmethod sink Atom [^Atom a & opts]
+  (let [ch (async/chan)]
+    (future
+      (loop-channel-to-sink ch #(swap! a conj %) opts)
+      (println :atom-sink-closed))
+    ch))
+
+(defn to!
+  "Starts sending data from the specified input channel to the specified sink(s).
+
+  | key                  | default | description |
+  |:---------------------|:--------|:------------|
+  | `:close-sinks?`      | `true`  | Whether to close all sinks once the input channel closes. |"
+  [channel sinks & {:keys [close-sinks?]
+                    :or   {close-sinks? true}}]
+  (future
+    (try
+      (loop [next (async/<!! channel)]
+        (when next
+          (doseq [sink sinks] (async/>!! sink next))
+          (recur (async/<!! channel))))
+
+      (println :sending-to-sinks-completed)
+
+      (catch Exception e
+        (log/error e))
+
+      (finally
+        (when close-sinks?
+          (doseq [sink sinks] (async/close! sink)))))))
--- test/cljka/channel_test.clj
+++ test/cljka/channel_test.clj
@@ -1,5 +1,6 @@
 (ns cljka.channel-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.core.async.impl.protocols :as async-protocols]
+            [clojure.test :refer :all]
             [clojure.core.async :as async]
             [cljka.channel :as channel]))
 
@@ -11,3 +12,68 @@
 (deftest closed?-returns-false-if-channel-open
   (let [ch (async/chan)]
     (is (false? (channel/closed? ch)))))
+
+(deftest can-send-channel-to-atom-sink
+  ; GIVEN an input channel
+  (let [ch   (async/chan 10)
+        data (vec (range 0 9))
+        a    (atom [])
+        sink (channel/sink a)]
+    ; AND an atom sink
+    (channel/to! ch [sink])
+
+    ; WHEN data arrives in the input channel
+    (doseq [x data]
+      (async/>!! ch x))
+
+    (let [fut (future (while (not= data @a)
+                        (Thread/sleep 100)))]
+      (deref fut 3000 nil))
+
+    ; THEN eventually the same data arrives at the sink
+    (is (= data @a))))
+
+(deftest sink-channels-close-once-input-channel-closes
+  ; GIVEN an input channel
+  (let [ch    (async/chan)
+        a1    (atom [])
+        a2    (atom [])
+        sink1 (channel/sink a1)
+        sink2 (channel/sink a2)]
+    ; AND sinks
+    (channel/to! ch [sink1 sink2])
+
+    (is (false? (async-protocols/closed? sink1)))
+    (is (false? (async-protocols/closed? sink2)))
+
+    ; WHEN the input channel closes
+    (async/close! ch)
+
+    (let [fut (future (while (or (not (async-protocols/closed? sink1))
+                                 (not (async-protocols/closed? sink2)))
+                        (Thread/sleep 100)))]
+      (deref fut 1000 nil))
+
+    ; THEN the sinks are eventually closed
+    (is (true? (async-protocols/closed? sink1)))
+    (is (true? (async-protocols/closed? sink2)))))
+
+(deftest can-stop-sink-channel-closing-once-input-channel-closes
+  ; GIVEN an input channel
+  (let [ch   (async/chan)
+        a    (atom [])
+        sink (channel/sink a)]
+    ; AND a sink with an explicit instruction NOT to close it
+    (channel/to! ch [sink] {:close-sinks? false})
+
+    (is (false? (async-protocols/closed? sink)))
+
+    ; WHEN the input channel closes
+    (async/close! ch)
+
+    (let [fut (future (while (not (async-protocols/closed? sink))
+                        (Thread/sleep 100)))]
+      (deref fut 1000 nil))
+
+    ; THEN the sink channel does not close
+    (is (false? (async-protocols/closed? sink)))))
