cdc350865e9cee32feb2a12c16608626df697ea9
Alistair Dutton <kelveden@gmail.com>
2024-01-21 16:20:25 +0000

    Test tweaks

--- test/cljka/config_test.clj
+++ test/cljka/config_test.clj
@@ -3,7 +3,21 @@
             [cljka.config :refer [->kafka-config with-principal]]))
 
 
-(deftest can-load-kafka-config-by-topic
+(deftest principal-kafka-config-is-merged
+  (let [config {:environments {:my-env {:kafka      {:core-field1 "core-value1"
+                                                     :core-field2 "core-value2"}
+                                        :principals {:my-user {:kafka {:user-field1 "value1"
+                                                                       :user-field2 "value2"}}}
+                                        :topics     {:topic1 {:name "sometopic"}}}}}]
+    (is (= {"core-field1" "core-value1"
+            "core-field2" "core-value2"
+            "user-field1" "value1"
+            "user-field2" "value2"}
+           (with-principal
+             :my-user
+             (->kafka-config config :my-env :topic1))))))
+
+(deftest principal-by-topic-kafka-config-is-merged
   (let [config {:environments {:my-env {:kafka      {:core-field1 "core-value1"
                                                      :core-field2 "core-value2"}
                                         :principals {:my-user {:kafka {:user-field1 "value1"
@@ -14,20 +28,6 @@
             "user-field1" "value1"
             "user-field2" "value2"}
            (->kafka-config config :my-env :topic1)))))
-
-(deftest can-load-kafka-config-by-principal
-  (let [config {:environments {:my-env {:kafka      {:core-field1 "core-value1"
-                                                     :core-field2 "core-value2"}
-                                        :principals {:my-user {:kafka {:user-field1 "value1"
-                                                                       :user-field2 "value2"}}}
-                                        :topics     {:topic1 {:principal :my-user}}}}}]
-    (is (= {"core-field1" "core-value1"
-            "core-field2" "core-value2"
-            "user-field1" "value1"
-            "user-field2" "value2"}
-           (with-principal
-             :my-user
-             (->kafka-config config :my-env :topic1))))))
 
 (deftest kafka-config-defaults-to-core
   (let [config {:environments {:my-env {:kafka {:core-field1 "core-value1"
